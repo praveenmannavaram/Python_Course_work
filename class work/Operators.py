e76cdd6e6926c5b7142dc1920028b168c64cbc25
praveenmannavaram <praveen.mannavaram@gmail.com>
2025-11-12 10:30:57 +0530

    First Program

---
+++ class work/Operators.py
@@ -0,0 +1,9 @@
+a = 20
+b = 10
+print("a+b:",a+b)   #a+b: 30
+print("a-b:",a-b)   #a-b: 10
+print("a*b:",a*b)   #a*b: 200
+print("a**b:",a**b) #a**b: 10240000000000
+print("a/b:",a/b)   #a/b: 2.0
+print("a//b:",a//b) #a//b: 2
+print("a%b:",a%b)   #a%b: 0
